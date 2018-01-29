30d1009be82c36d39ad207421d0eeb15c0ae8484
Tianyu Duan <tianyuduan1991@gmail.com>
2018-01-29 14:25:24 -0800

    starting react(review) building Youtube Components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from 'react' //core library, works with components
+import ReactDOM from 'react-dom'// needs library to render to DOM
+//create new compnents
+//this compoent should create some HTML
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = function () {
+  return  <div>Hi!</div>
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () => {
+  return <div>hello! </div>
+}
+
+// takes this component's generated HTML and puts it on the page
+
+// when create compoennt, it makes a class of the component, think of the function app as a class 
+ // this will actually makes an instance when run thru Babel
+ReactDOM.render(<App />, document.querySelector('.container'))
